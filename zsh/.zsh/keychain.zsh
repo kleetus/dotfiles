78f2bc64a960b66cf07992d3408aa5f25d97e50e
k <k@node1.lan.local>
2019-06-13 12:26:01 +0000

    Personal alts.

--- zsh/.zsh/keychain.zsh
+++ zsh/.zsh/keychain.zsh
@@ -15,10 +15,7 @@
 # ░░░░░░░░░░
 #
 #█▓▒░ ssh keys
-export SSH_KEY_PATH="~/.ssh/personal"
-
-#█▓▒░ funtoo keychain
-eval `keychain -q --agents ssh,gpg --eval ~/.ssh/personal 0x0DA7AB45AC1D0000`
+export SSH_KEY_PATH="~/.ssh"
 
 #█▓▒░ gpg agent
 GPG_TTY=$(tty)
